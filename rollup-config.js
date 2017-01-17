3c02455a5b34ef0b9544ca5b3fc42de5a89eae63
Sam Noedel <sam.noedel@gmail.com>
2017-01-17 06:55:27 -0800

    Add AoT compilation

---
+++ rollup-config.js
@@ -0,0 +1,24 @@
+import rollup      from 'rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'output/app/main-aot.js',
+  dest: 'dist/oscars.min.js',
+  sourceMap: false,
+  format: 'iife',
+  plugins: [
+    nodeResolve({ jsnext: true, module: true }),
+    commonjs({
+      include: [
+        'node_modules/rxjs/**',
+        'node_modules/eventemitter3/**'
+      ],
+      namedExports: {
+        'node_modules/eventemitter3/index.js': [ 'EventEmitter' ]
+      }
+    }),
+    uglify()
+  ]
+}
