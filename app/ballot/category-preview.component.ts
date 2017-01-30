f41a28cfa21ee7d150e143e69a35f0658b973cad
codifiman <c.fiman@f5.com>
2017-01-29 20:11:38 -0800

    Add styling for a ballot category preview; added helper functions for retrieving colors

--- app/ballot/category-preview.component.ts
+++ app/ballot/category-preview.component.ts
@@ -3,8 +3,19 @@ import { Category } from '../shared/model';
 
 @Component({
   selector: 'o-category-preview',
+  styleUrls: ['output/ballot/category-preview.css'],
   template: `
-    <h3>{{ category.name }}</h3>
+    <div>
+      <div class='text-container'>
+        <div class='left-content'>
+          <h4>Cast your vote for</h4>
+          <h2>{{ category.name }}</h2>
+        </div>
+        <div class='right-content'>
+          <div class='right-arrow'></div>
+        </div>
+      </div>
+    </div>
     `
 })
 export class CategoryPreviewComponent {
