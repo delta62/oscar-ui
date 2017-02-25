02587fff099f78a8fcf8fce15e07e976d29d4727
codifiman <c.fiman@f5.com>
2017-02-24 20:49:57 -0800

    Add title to header; style score; add header to scoreboard

--- app/scoreboard/scoreboard.component.ts
+++ app/scoreboard/scoreboard.component.ts
@@ -4,11 +4,16 @@ import { ScoreStore, AccountStore } from '../shared/stores';
 import { UserScore, User } from '../shared/model';
 
 @Component({
+  moduleId: module.id,
+  styleUrls: [ './scoreboard.css' ],
   selector: 'o-scoreboard',
   template: `
     <o-header [score]="score"></o-header>
-    <o-sub-header title="Scoreboard"></o-sub-header>
-    <router-outlet></router-outlet>`
+    <div class='body'>
+      <o-sub-header title="Scoreboard"></o-sub-header>
+      <router-outlet></router-outlet>
+      <o-copyright></o-copyright>
+    </div>`
 })
 export class ScoreboardComponent implements DoCheck {
   score: UserScore;
--- app/shared/header.component.ts
+++ app/shared/header.component.ts
@@ -10,9 +10,20 @@ import { LogoutPayload, MenuTogglePayload } from './payload';
   selector: 'o-header',
   styleUrls: [ './header.css' ],
   template: `
+    <div class='left-content'>
       <span (click)="onMenuClick()" class='open-btn'>…</span>
-      <span (click)="onScoreClick()">{{ score.score.totalScore }}</span>
-    `
+    </div>
+    <div class='right-content'>
+      <div class='score' (click)="onScoreClick()">
+        <h3>{{ score.score.totalScore }}</h3>
+        <h6>Score</h6>
+      </div>
+      <div class='title' (click)="onTitleClick()">
+        <h6>The</h6>
+        <h5>Oscars</h5>
+        <h5>Ballot</h5>
+      </div>
+    </div>`
 })
 export class HeaderComponent {
   @Input() score: UserScore;
@@ -23,6 +34,10 @@ export class HeaderComponent {
 
   onScoreClick(): void {
     this.router.navigateByUrl('/scoreboard');
+  }
+
+  onTitleClick(): void {
+    this.router.navigateByUrl('/ballot');
   }
 
   onMenuClick(): void {
