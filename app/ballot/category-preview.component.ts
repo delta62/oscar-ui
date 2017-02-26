cdad023c8178a80afc5354e6e35cb3c969eb3468
codifiman <c.fiman@f5.com>
2017-02-26 08:06:07 -0800

    Fix overflow-x in category; change prefix on category preview once a response has been given and show the response value

--- app/ballot/category-preview.component.ts
+++ app/ballot/category-preview.component.ts
@@ -9,8 +9,9 @@ import { Category, Response } from '../shared/model';
     <div class='category' [ngStyle]="{'background-image': slug}">
       <div class='text-container'>
         <div class='left-content'>
-          <h4>Cast your vote for</h4>
+          <h4>{{ prefix }}</h4>
           <h3>{{ category.name }}</h3>
+          <h6 class='choice'>{{ choice }}</h6>
         </div>
         <div class='right-content'>
           <div class='arrow right'></div>
@@ -27,5 +28,14 @@ export class CategoryPreviewComponent {
       const slug = this.response.value.toLowerCase().replace(/[^\w\s]/g, '').replace(/\s/g, '-');
       return 'url(images/' + slug + '-wide.jpg)';
     }
+  }
+
+  get prefix(): string {
+    if (this.response) { return "Your choice for"; }
+    else { return "Cast your vote for"; }
+  }
+
+  get choice(): string {
+    if (this.response) { return this.response.value }
   }
 }
