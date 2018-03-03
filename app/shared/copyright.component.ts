fe1c8d6269888f29c8d5e64bd136664c0c137eb8
Sam Noedel <sam.noedel@gmail.com>
2018-03-03 08:26:56 -0800

    Update year in UI

--- app/shared/copyright.component.ts
+++ app/shared/copyright.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './copyright.css' ],
   selector: 'o-copyright',
   template: `
-    &copy; Sam & Codi Noedel, 2017
+    &copy; Sam & Codi Noedel, 2017 - 2018
   `
 })
 export class CopyrightComponent {  }
